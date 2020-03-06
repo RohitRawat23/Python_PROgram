8361995209f83a62c92e3d4d0eee49054bc97a57
rohit rawat <rohitrawatindian@gmail.com>
2020-03-06 13:50:51 +0530

    Added a program of string using format string.

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -24,6 +24,19 @@ s4 = s1 + s2 + s3
 print(s4)
 
 
+# default arguments
+print("Hello {}, your balance is {}.".format("Adam", 230.2346))
+
+# positional arguments
+print("Hello {0}, your balance is {1}.".format("Adam", 230.2346))
+
+# keyword arguments
+print("Hello {name}, your balance is {blc}.".format(name="Adam", blc=230.2346))
+
+# mixed arguments
+print("Hello {0}, your balance is {blc}.".format("Adam", blc=230.2346))
+
+
 
 
 
